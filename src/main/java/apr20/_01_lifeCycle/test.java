f994fc2544c914e891001a802b75018d12451bec
Raystar <raystar9@naver.com>
2018-04-22 13:13:05 +0900

    finish result page

--- src/main/java/apr20/_01_lifeCycle/test.java
+++ src/main/java/apr20/_01_lifeCycle/test.java
@@ -22,31 +22,29 @@ public class test extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html; charset=utf-8");
-		request.setCharacterEncoding("utf-8");
-		response.setCharacterEncoding("utf-8");
-		response.getWriter().println("아이디 : " + request.getParameter("id") + "<br>");
-		response.getWriter().println("비밀번호 : " + request.getParameter("pass"));
-		response.getWriter().println("주민등록번호 : " + request.getParameter("jumin1") + "-" + request.getParameter("jumin2"));
-		response.getWriter().println("e-mail : " + request.getParameter("email") + "@" + request.getParameter("domain"));
-		response.getWriter().println("성별 : " + request.getParameter("gender"));
-		
-		response.getWriter().print("취미 : ");
-		for(String str : request.getParameterValues("hobby")) {
-			response.getWriter().print(str + " ");
-		}
-		response.getWriter().close();
-	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		response.setContentType("text/html; charset=utf-8");
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
+		response.getWriter().print("아이디 : " + request.getParameter("id") + "<br>");
+		response.getWriter().print("비밀번호 : " + request.getParameter("pass")+ "<br>");
+		response.getWriter().print("주민등록번호 : " + request.getParameter("jumin1") + "-" + request.getParameter("jumin2")+ "<br>");
+		response.getWriter().print("e-mail : " + request.getParameter("email") + "@" + request.getParameter("domain")+ "<br>");
+		response.getWriter().print("성별 : " + request.getParameter("gender")+ "<br>");
+		
+		response.getWriter().print("취미 : ");
+		for(String str : request.getParameterValues("hobby")) {
+			response.getWriter().print(str + " ");
+		}
+		
+		response.getWriter().print("<br>우편 번호 : " + request.getParameter("post1") + "-" + request.getParameter("post2") + "<br>");
+		response.getWriter().print("주소 : " + request.getParameter("address") + "<br>");
+		response.getWriter().print("자기소개 : " + request.getParameter("introduce") + "<br>");
+		response.getWriter().close();
 	}
 
 }
